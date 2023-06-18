88304582106ab013126b02e001e917e75a90796a
KeeanByrne <keeanbyrne00@gmail.com>
2023-06-18 12:43:02 +0200

    Add project files.

---
+++ Flappy Bird/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Flappy_Bird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtScore = new System.Windows.Forms.Label();
+            this.flappyBird = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.pipeBottom = new System.Windows.Forms.PictureBox();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft YaHei", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(12, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(170, 50);
+            this.txtScore.TabIndex = 4;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // flappyBird
+            // 
+            this.flappyBird.Image = global::Flappy_Bird.Properties.Resources.bird;
+            this.flappyBird.Location = new System.Drawing.Point(62, 159);
+            this.flappyBird.Name = "flappyBird";
+            this.flappyBird.Size = new System.Drawing.Size(83, 65);
+            this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.flappyBird.TabIndex = 3;
+            this.flappyBird.TabStop = false;
+            this.flappyBird.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // ground
+            // 
+            this.ground.Image = global::Flappy_Bird.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(-7, 508);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(670, 69);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 2;
+            this.ground.TabStop = false;
+            // 
+            // pipeBottom
+            // 
+            this.pipeBottom.Image = global::Flappy_Bird.Properties.Resources.pipe;
+            this.pipeBottom.Location = new System.Drawing.Point(343, 303);
+            this.pipeBottom.Name = "pipeBottom";
+            this.pipeBottom.Size = new System.Drawing.Size(100, 199);
+            this.pipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom.TabIndex = 1;
+            this.pipeBottom.TabStop = false;
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Image = global::Flappy_Bird.Properties.Resources.pipedown;
+            this.pipeTop.Location = new System.Drawing.Point(373, 9);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(100, 143);
+            this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop.TabIndex = 0;
+            this.pipeTop.TabStop = false;
+            this.pipeTop.Click += new System.EventHandler(this.pipeTop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(661, 570);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.flappyBird);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipeBottom);
+            this.Controls.Add(this.pipeTop);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird Game";
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox pipeBottom;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox flappyBird;
+        private System.Windows.Forms.Label txtScore;
+    }
+}
+
